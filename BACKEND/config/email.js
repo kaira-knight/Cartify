1b98fb3853d0251f549e229d1b776ea8001ffe5a
Manya537 <168168681+Manya537@users.noreply.github.com>
2026-03-21 12:03:28 +0530

    Update email.js
    c

--- BACKEND/config/email.js
+++ BACKEND/config/email.js
@@ -1,11 +1,11 @@
 import nodemailer from "nodemailer";
-
-const transporter=nodemailer.createTransport({
-    service:"gmail",
-    auth: {
-        user:process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  port: 465,
+  secure: true, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS,
+  },
 });
-
 export default transporter;
