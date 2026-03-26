84dd4be2e02127cc9511f8b40d61dabd0b511bbc
Manya537 <168168681+Manya537@users.noreply.github.com>
2026-03-26 22:25:34 +0530

    c

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const googleAuth = async (token) => {
   const res = await axios.post(
-    "http://localhost:8000/api/google-login",
+    "http://localhost:8000/api/auth/google-login",
     { token }
   );
   return res.data;
